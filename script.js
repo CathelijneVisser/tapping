417daeb38abf26b8e4f936d8ead637262cd01106
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-09-25 10:10:26 +0200

    speed works

--- script.js
+++ script.js
@@ -16,29 +16,30 @@ function random_item(array) {
 }
 
 function activate() {
-    if (array.length > 0) {       
+    if (array.length > 0) {
         var random = random_item(array)
-        random.classList.add("active") 
-        for( var i = 0; i < array.length; i++){ 
-                                    
-            if ( array[i] === random) { 
-                array.splice(i, 1); 
-                i--; 
+        random.classList.add("active")
+        for (var i = 0; i < array.length; i++) {
+
+            if (array[i] === random) {
+                array.splice(i, 1);
+                i--;
             }
         }
     }
 }
+
 //change speed
 const slider = document.querySelector(".speed")
-var value = slider.value + "00"
+var TimerID = null
 
 slider.addEventListener("change", () => {
-    value = slider.value + "00"
-    // console.log(value)
+    clearInterval(TimerID)
+    value = slider.value
+    TimerID = setInterval(activate, value)
 })
 
-console.log(value)
-setInterval(activate, value)
+
 
 //change color
 const input = document.querySelector(".color")
@@ -47,13 +48,13 @@ const colorUpdate = (cssVars) => {
     const root = document.querySelector(":root")
     const keys = Object.keys(cssVars)
     keys.forEach((key) => {
-      root.style.setProperty(key, cssVars[key])
+        root.style.setProperty(key, cssVars[key])
     })
-  }
-  
+}
+
 input.addEventListener("input", (e) => {
     const cssPropName = `--color${e.target.getAttribute("data-id")}`
     colorUpdate({
-    [cssPropName]: e.target.value
+        [cssPropName]: e.target.value
     })
 })
